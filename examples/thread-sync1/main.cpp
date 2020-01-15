3c095cae00a6ff0c3d8da76d30ef3ecf3fc2c9e5
V <no-email>
2020-01-15 22:10:45 +0900

    + example:  mutex

---
+++ examples/thread-sync1/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <mutex>
+using namespace std;
+
+/* Синхронизация потоков при общем доступе для записи в переменную */
+
+int shared_var = 0;
+
+void foo_no_sync(){
+    this_thread::sleep_for(chrono::milliseconds(rand()%10));
+    for (unsigned i =0; i<100;i++) {
+        shared_var++;
+    }
+}
+
+mutex mtx;
+
+void foo_sync(){
+    this_thread::sleep_for(chrono::milliseconds(rand()%10));
+
+    // поток блокирует (отмечает) мьютекс.
+    // Остальные потоки будут ждать в строке ниже
+    mtx.lock();
+
+    // Мьютекс заблокирован. Никто не помешает изменять переменную.
+    for (unsigned i =0; i<100;i++) {
+        shared_var++;
+    }
+
+    // работа с переменной завершена, освободим
+    mtx.unlock();
+}
+
+
+int main()
+{
+    unsigned Nth = 10000; // количество потоков
+    vector<thread> threads_poll(Nth);
+
+    cout << "Starting " << Nth << " threads" << endl;
+
+    for (unsigned i = 0; i < Nth; i++){
+//        threads_poll[i] = thread(foo_no_sync); // изменение переменной без синхронизации
+
+        threads_poll[i] = thread(foo_sync);      // изменение переменной с синхронизацией
+    }
+
+    cout << "waiting for threads termination..." << endl;
+    for(unsigned i = 0; i < Nth; i++){
+        threads_poll[i].join();
+    }
+
+    cout << "shared_var = " << shared_var << endl;
+
+    return 0;
+}
+
+
+
+// см. еще один пример использования мьютекса в документации стандартной библиотеки:
+// http://www.cplusplus.com/reference/mutex/mutex/
---
+++ examples/thread-sync1/thread-sync1.pro
@@ -0,0 +1,7 @@
+TEMPLATE = app
+CONFIG += console c++11 thread
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+        main.cpp
