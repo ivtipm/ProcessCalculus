db89a056317272086062c016139e6c293ed5aabf
V <V>
2021-04-14 11:58:13 +0900

    ..

--- OpenMP.md
+++ OpenMP.md
@@ -8,7 +8,7 @@ OpenMP (Open Multi-Processing) — открытый стандарт для ра
 
 - Поддерживается многими современными компиляторами.
 
-- Упрощает создание потоков.
+- Сильно упрощает создание потоков и распределение между ними вычислений.
 
 Вычисления распараллеливаются между несколькими потоками, включая основновной, master поток:
 ![](https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Fork_join.svg/1280px-Fork_join.svg.png)
@@ -19,7 +19,7 @@ OpenMP (Open Multi-Processing) — открытый стандарт для ра
 ![](https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/OpenMP_language_extensions.svg/1280px-OpenMP_language_extensions.svg.png)
 
 
-```
+```C++
 #include <omp.h>
 
 int main(){
@@ -33,7 +33,63 @@ int main(){
   return 0;}
 ```
 
-Пример: 
 
 
+#### Общие и локальные переменные для потоков
+Все переменные, созданные до директивы parallel
+являются общими для всех потоков. Переменные, созданные внутри потока являются локальными (приватными) и доступны только текущему потоку.
+
+Общие переменные при этом не потокобезопасны:
+```C++
+int value = 123;
+
+#
+#pragma omp parallel
+{
+  value++;
+  std::cout << value++ << std::endl;
+}
+```
+
+Пример:
 https://lms-vault.s3.amazonaws.com/private/1/courses/2016-spring/spb-hp-course/materials/Lecture_OpenMP_Summer2008_uL0moR8.pdf?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAUKOEY5ZX6VXK3RWN%2F20210413%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20210413T172319Z&X-Amz-Expires=10&X-Amz-SignedHeaders=host&X-Amz-Signature=b5212372826ad3e7512514259d3f4c53e88dd07edf1bc8d1cf796bc3b8b5abd8
+
+
+#### Директивы компилятора для || вычислений
+`#pragma omp ... `
+
+Для распараллеливания вычислений:
+- `parallel` 	Defines a parallel region, which is code that will be executed by multiple threads in parallel.
+- `for` 	Causes the work done in a for loop inside a parallel region to be divided among threads.
+- `sections` 	Identifies code sections to be divided among all threads.
+- `single` 	Lets you specify that a section of code should be executed on a single thread, not necessarily the master thread.
+
+
+**Общие пепеременные** -- как глобальные переменные, доступны всем потокам
+```C++
+#pragma omp parallel shared(A, B, C)
+```
+
+**Локальные пепеременные** -- у каждого потока своя копия переменной. Даже если переменная объявлена в общей области видимости.
+```C++
+#pragma omp parallel private(A, B, C)
+```
+
+
+#### Директивы компилятора 
+For master and synchronization:
+
+`master` 	Specifies that only the master thread should execute a section of the program.
+`critical` 	Specifies that code is only executed on one thread at a time.
+`barrier` 	Synchronizes all threads in a team; all threads pause at the barrier, until all threads execute the barrier.
+`atomic` 	Specifies that a memory location that will be updated atomically.
+`flush` 	Specifies that all threads have the same view of memory for all shared objects.
+`ordered` 	Specifies that code under a parallelized for loop should be executed like a sequential loop.
+
+For data environment:
+`threadprivate` 	Specifies that a variable is private to a thread.
+
+https://docs.microsoft.com/ru-ru/cpp/parallel/openmp/reference/openmp-directives?view=msvc-160
+
+# Ссылки
+- https://www.openmp.org//
--- examples/openMP_1/main.cpp
+++ examples/openMP_1/main.cpp
@@ -1,18 +1,169 @@
 #include <iostream>
+#include <math.h>
+#include <vector>
+#include <chrono>
+#include <omp.h>
 // см. также pro файл
 
 using namespace std;
 
-int main()
-{
-    // запуск кода в фигурных скобках в 4 потока
-    // на экран надпись будет выведена 4 раза
+// простой пример распараллеливания кода
+void example1_hello_world(){
+    // напечатаем текущее число потоков
+    cout << "threads: " << omp_get_num_threads() << endl;                  // 1
+
+    // запуск кода в фигурных скобках в 4 потока -- он выполнится 4 раза.
     #pragma omp parallel num_threads(4)
     {
-    cout << "Hello, || World!" << endl;
+//    cout << "threads: " << omp_get_num_threads() << endl;                // 4
+    cout << "Hello, || World from thread" << omp_get_thread_num() << "!" << endl;
     }
 
+    cout << "threads: " << omp_get_num_threads() << endl;                   //  1
     printf("Done");
+}
+
+
+// распараллеливание цикла
+void example2_ll_for(){
+
+    #pragma omp parallel for
+    for (unsigned i = 0; i< 100; i++) {
+        cout << i << endl;
+    }
+
+    // pragma omp parallel for:
+    // 1. определит оптимальное число потоков
+    // 2. сделает из тела цикла отдельную функцию
+    // 3. разделит общее число итераций цикла между потоками
+}
+
+
+// явное || разных блоков кода
+void example3_ll_sections(){
+
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        for (unsigned i = 0; i<100; i+=2)
+            cout << i << endl;
+
+        #pragma omp section
+        for (unsigned i = 1; i<100; i+=2)
+            cout << i << endl;
+    }
+
+    // pragma omp parallel for:
+    // 1. определит оптимальное число потоков
+    // 2. сделает из тела цикла отдельную функцию
+    // 3. разделит общее число итераций цикла между потоками
+}
+
+
+
+void example4_shared_private_atomic_reduction(){
+    double sum = 0.0;
+
+    // плохой пример
+    #pragma omp parallel for private(sum)
+    for (unsigned i = 0; i< 100; i++) {
+        sum += 1;
+    }
+    cout << "private sum: " << sum << endl;
+
+    // плохой пример
+    sum = 0;
+    #pragma omp parallel for shared(sum)
+    for (unsigned i = 0; i< 100; i++) {
+        sum += 1;
+    }
+    cout << "shared sum: " << sum << endl;
+
+    // медленный пример
+    sum = 0;
+    #pragma omp parallel for
+    for (unsigned i = 0; i< 100; i++) {
+        #pragma omp atomic
+        sum += 1;
+    }
+    cout << "shared atomic sum: " << sum << endl;
+
+    // private -- у каждого потока будет своя, локальная, копия переменной
+    // shared -- переменная будет общей для всех потоков, но ей изменение не потокобезопасно
+
+    // #pragma omp atomic  -- сделать следующий за директивой оператор присваивания атомарным.
+    // потоки будут ждать завершения оператора присваивания
+    // при этом правая часть оператора = может выполнятся ||
+
+
+    // хороший пример
+    sum = 0;
+    #pragma omp parallel for reduction(+:sum)
+    for (unsigned i = 0; i< 100; i++) {
+        sum += 1;
+    }
+    cout << "reduction sum: " << sum << endl;
+
+//    reduction(+:sum):
+   // сделает локальную копию sum в каждом потоке
+   // после завершения всех потоков выполнит указанное действие (+) для всех локальных копий
+}
+
+using vector_int = vector<int>;
+
+double sum_l(unsigned long n){
+    double s = 0;
+    for(unsigned long i=0; i<n; i++){
+        s += sin(i);
+    }
+
+    return s;
+}
+
+
+double sum_ll(unsigned long n){
+
+    double s = 0;
+    #pragma parallel omp for num_threads(6) // reduction(+:s)
+    for(unsigned long i=0; i<n; i++){
+        s += sin(i % 100);
+    }
+
+    return s;
+}
+
+
+void print_array(vector_int *v){
+    for(unsigned long i=0; i<v->size(); i++){
+        cout << v->at(i) << " ";
+    }
+}
+
+int main()
+{
+    cout << "max threads: " << omp_get_max_threads() << endl;
+
+//    example1_hello_world();         // hello, || world!
+//    example2_ll_for();              // распараллеливание цикла
+//    example3_ll_sections();
+//    example4_shared_private_atomic_reduction();
+
+    const unsigned long N = 1e9/3;
+    auto t0 = chrono::system_clock::now();
+    sum_l(N);
+    auto t1 = chrono::system_clock::now();
+    cout << endl << "dt = " << chrono::duration_cast<chrono::milliseconds>(t1 - t0).count()
+              << "ms" <<endl;
+
+
+    t0 = chrono::system_clock::now();
+    sum_ll(N);
+    t1 = chrono::system_clock::now();
+    cout << endl << "dt = " << chrono::duration_cast<chrono::milliseconds>(t1 - t0).count()
+              << "ms" <<endl;
+
+//    print_array(v);
+
     return 0;
 }
 
