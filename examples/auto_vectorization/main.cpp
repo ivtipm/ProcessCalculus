7b0e526c769082f7f5a391933eca3f0e70389898
VetrovSV <32772127+VetrovSV@users.noreply.github.com>
2023-04-21 15:46:45 +0900

    Update main.cpp

--- examples/auto_vectorization/main.cpp
+++ examples/auto_vectorization/main.cpp
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <iostream>
 #include <chrono>
 
@@ -11,7 +12,7 @@ int main()
     auto t0 = system_clock::now();
 
 
-    const unsigned N = 1024*1024*512;
+    const unsigned N = 1024*1024*32;
     double *a, *b, *c;
 
     a = new double[N];
@@ -19,9 +20,9 @@ int main()
     c = new double[N];
 
     for (unsigned i = 0; i<N; i++)
-        c[i] = a[i] * b[i];
+        c[i] = pow( 1.0/(1 + exp(a[i])) * 1/(1+exp(b[i])), 0.3);
 
-    cout << c[random()%N];
+    cout << c[rand()%N];
     auto t1 = system_clock::now();
     cout << "dt = " << chrono::duration_cast<chrono::milliseconds>(t1 - t0).count() << "ms" <<endl;
 
@@ -34,7 +35,7 @@ int main()
 // g++ main.cpp -o no_vectorization
 
 // Вариант 2, оптимизация (включая векторизацияю):
-// g++ main.cpp -o no_vectorization -O3
+// g++ main.cpp -o vectorization -O3
 
 // Примеры приведены для linux. В Windows имя исполняемого файла компилятора отличается (см. MinGW)
 // g++ main.cpp -o no_vectorization
@@ -47,6 +48,3 @@ int main()
 
 // Больше примеров опций векторизации: https://stackoverflow.com/questions/29292818/how-to-vectorize-my-loop-with-g
 //https://hpac.cs.umu.se/teaching/sem-accg-16/slides/08.Schmitz-GGC_Autovec.pdf
-
-
-
