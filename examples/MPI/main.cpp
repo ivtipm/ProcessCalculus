91f7013c592b4e2bb23ed4d6358e081d9d9f18c6
vetrovsv <eridani_alpha@hotmail.com>
2023-04-28 01:56:54 +0900

    ..

--- examples/MPI/main.cpp
+++ examples/MPI/main.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {   
+    
     // Initialize enviroment
     MPI_Init(nullptr, nullptr);
 
@@ -11,7 +12,7 @@ int main()
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // Get the rank of the process
+    // Get the rank (number) of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
@@ -19,13 +20,14 @@ int main()
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     int name_len;
     MPI_Get_processor_name(processor_name, &name_len);
-
+    
     // Print off a hello world message
     printf("Hello world from processor %s, rank %d out of %d processors\n",
                processor_name, world_rank, world_size);
 
     // Finalize the MPI environment.
     MPI_Finalize();
+    
 }
 
 
@@ -34,6 +36,7 @@ int main()
 
 // Установка MPICH в Debian:
 // sudo apt install mpich
+// может потребоваться перезапуск ОС?
 
 // Проверка:
 // mpic++ -v         # информация о версии
@@ -43,8 +46,7 @@ int main()
 // -o main — имя выходного (исполняемого) файла
 
 // Запуск:
-// mpi
-
+// mpirun -n 10 ./main
 
 
 // подробности о MPI см в файле MPI.md
