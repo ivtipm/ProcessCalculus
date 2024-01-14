baa2df699806df1504a7c4758d2eeb803787fa10
vetrovsv <eridani_alpha@hotmail.com>
2024-01-15 01:15:59 +0900

    + C# async thread example

---
+++ examples/Csharp_async/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;                       // Для запуска новых потоков
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Csharp_async
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        PiCalc pi_calc = new PiCalc();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // получим количество ядер (sic) процессора
+            label_cores_n.Content = Environment.ProcessorCount.ToString();
+        }
+
+
+        /// <summary> Запуск вычислений в текущем потоке. Блокирует цикл обработки событий -> взаимодейтсвие с окном будет недоступно до завершения метода. </summary>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            pi_calc.N = Convert.ToUInt64(Convert.ToDouble(textbox_n.Text))*1000_000;   // todo: сделать проверку
+
+            pi_calc.calc();
+
+            textbox_result.Text = pi_calc.get_pi().ToString();
+        }
+
+        /// <summary> Запуск вычислений в отдельном, новом потоке. Не блокирует цикл обработки событий</summary>
+        private async void btn_run_sep_thread_Click(object sender, RoutedEventArgs e){
+            pi_calc.N = Convert.ToUInt64(Convert.ToDouble(textbox_n.Text)) * 1000_000;   // todo: сделать проверку
+
+            // async позволяет приостановить выполнение метода, сохранить его состояние,
+            // до тех пор пока операция await запущенная в параллельном потоке, не завершится
+            btn_run_sep_thread.IsEnabled = false;
+            var result = await Task.Run( () => pi_calc.calc() );
+            // var вместо типа - сообщение компилятору самостоятельно вывести тип
+            // await неблокирующее ожидание завершения операции 
+            // () => pi_calc.calc() -- анонимная функция без параметров, тело функции приведено после =>
+
+            textbox_result.Text = pi_calc.get_pi().ToString();
+            btn_run_sep_thread.IsEnabled = true;
+        }
+    }
+}
+
+// todo: остановка задачи
+// todo: замер времени выполнения
---
+++ examples/Csharp_async/PiCalc.cs
@@ -0,0 +1,46 @@
+﻿/// Модуль для вычислений
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_async
+{   
+   /// <summary>
+   /// Вычисляет число Пи методом Монте-Карло
+   /// </summary>
+    class PiCalc
+    {
+        double pi;
+        /// <summary>
+        /// количество итераций для вычислений
+        /// </summary>
+        ulong n;
+
+        public ulong N { get => n;
+                         set { if (value > 0) n = value; } }
+
+        /// <summary> Вычисляет и возвращает число Пи </summary>
+        /// <returns> Вычисленное число Пи</returns>
+        public double calc() { pi = 0; 
+            // todo: пояснить
+            Random rnd = new Random();
+            ulong inside_sector = 0;
+            for (ulong i = 0; i<n; i++)
+            {
+                double x = rnd.NextDouble();
+                double y = rnd.NextDouble();
+
+                if (x*x + y*y <= 1.0)
+                    inside_sector++;
+            }
+            pi = Convert.ToDouble(inside_sector)/ Convert.ToDouble(n) * 4;
+            return pi; }
+
+        /// <returns> ранее вычисленное число Пи </returns>
+        public double get_pi() { return pi; }
+    }
+}
+
+/* Этот класс занимается только вычислениями и не управляет потоками */
