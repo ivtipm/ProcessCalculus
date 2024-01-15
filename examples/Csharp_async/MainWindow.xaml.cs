245e38cd75c4f1afe251ae377677688c23230e46
VetrovSV <32772127+VetrovSV@users.noreply.github.com>
2024-01-15 23:23:36 +0900

    upd: C# background thread with GUI: + progress bar

--- examples/Csharp_async/MainWindow.xaml.cs
+++ examples/Csharp_async/MainWindow.xaml.cs
@@ -30,13 +30,19 @@ namespace Csharp_async
             label_cores_n.Content = Environment.ProcessorCount.ToString();
         }
 
+        /// <summary> Метод обновления полосы прогресса </summary>
+        private void update()        {
+           // этот метод будет вызываться из другого потока (у которого нет доступа к данному объекту (окну)
+           // поэтому обратися к Диспетчеру, и поместим анонимную функцию в очередь обработки текущим для окна потоком 
+            Dispatcher.BeginInvoke(new Action(() => { progressBar.Value = progressBar.Value + 10; }));
+        }
 
         /// <summary> Запуск вычислений в текущем потоке. Блокирует цикл обработки событий -> взаимодейтсвие с окном будет недоступно до завершения метода. </summary>
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             pi_calc.N = Convert.ToUInt64(Convert.ToDouble(textbox_n.Text))*1000_000;   // todo: сделать проверку
 
-            pi_calc.calc();
+            pi_calc.calc( update );
 
             textbox_result.Text = pi_calc.get_pi().ToString();
         }
@@ -48,13 +54,22 @@ namespace Csharp_async
             // async позволяет приостановить выполнение метода, сохранить его состояние,
             // до тех пор пока операция await запущенная в параллельном потоке, не завершится
             btn_run_sep_thread.IsEnabled = false;
-            var result = await Task.Run( () => pi_calc.calc() );
+            progressBar.Value = 0;
+
+            var result = await Task.Run( () => pi_calc.calc(update) );
             // var вместо типа - сообщение компилятору самостоятельно вывести тип
             // await неблокирующее ожидание завершения операции 
             // () => pi_calc.calc() -- анонимная функция без параметров, тело функции приведено после =>
+            // update -- специальная callback функция, которая безопасно может быть вызвана из не основного потока
+            // по логике calc эта функция будет вызываться каждые 10% прогресса вычислений
 
             textbox_result.Text = pi_calc.get_pi().ToString();
             btn_run_sep_thread.IsEnabled = true;
+        }
+
+        private void btn_stop_Click(object sender, RoutedEventArgs e)
+        {
+            pi_calc.stop = true;
         }
     }
 }
